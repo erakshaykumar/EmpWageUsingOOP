f254a0fe6383702353d7c47315dd68d2bc9c37ee
erakshaykumar <akaakshay304@gmail.com>
2022-02-12 22:47:43 +0530

    Added: Welcome To Employee Wage Computation Problem

---
+++ src/com/bridgelabz/EmpWageUsingOOPS/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.EmpWageUsingOOPS;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Computation Using OOP's Concept");
+		}
+
+	}
