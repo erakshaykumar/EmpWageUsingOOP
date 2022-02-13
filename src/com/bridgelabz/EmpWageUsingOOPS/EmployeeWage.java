01b00fd0059ff2a6fd9d1ff65879054d8e256d72
erakshaykumar <akaakshay304@gmail.com>
2022-02-13 12:47:06 +0530

    Added: Employee Present Or Absent Logic

--- src/com/bridgelabz/EmpWageUsingOOPS/EmployeeWage.java
+++ src/com/bridgelabz/EmpWageUsingOOPS/EmployeeWage.java
@@ -1,9 +1,23 @@
 package com.bridgelabz.EmpWageUsingOOPS;
 
-public class EmployeeWage {
+import java.util.Random;
 
+public class EmployeeWage {
+	public static void EmpWageUC1() {
+		Random ran = new Random();
+		int present = 1;
+		int empCheck = ran.nextInt(2);
+		if (empCheck == present) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Using OOP's Concept");
-		}
+		EmpWageUC1();
+	}
 
 	}
