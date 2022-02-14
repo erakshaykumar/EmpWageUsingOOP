20a4a01040d566353fd63f95cf69b351d45e0760
erakshaykumar <akaakshay304@gmail.com>
2022-02-14 23:01:04 +0530

    added: employee salary for a day

--- src/com/bridgelabz/EmpWageUsingOOPS/EmployeeWage.java
+++ src/com/bridgelabz/EmpWageUsingOOPS/EmployeeWage.java
@@ -2,22 +2,33 @@ package com.bridgelabz.EmpWageUsingOOPS;
 
 import java.util.Random;
 
-public class EmployeeWage {
-	public static void EmpWageUC1() {
-		Random ran = new Random();
-		int present = 1;
-		int empCheck = ran.nextInt(2);
-		if (empCheck == present) {
-			System.out.println("Employee is Present");
-		}
-		else {
-			System.out.println("Employee is Absent");
-		}
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation Using OOP's Concept");
-		EmpWageUC1();
+class Employee {
+	static final int IS_FULL_TIME = 1;
+	static final int EMP_WAGE_PER_DAY = 20;
+
+	public static int empCheck() {
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		return empCheck;
 	}
 
+	public static void empSalary(int empCheck) {
+		int empWrkHrs = 0;
+		if (empCheck == IS_FULL_TIME) {
+			empWrkHrs = 8;
+		} else {
+			empWrkHrs = 0;
+		}
+		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
+		System.out.println("empWage = " + empWage);
 	}
+}
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Computation Using OOP's Concept");
+		int empCheck = Employee.empCheck();
+		Employee.empSalary(empCheck);
+	}
+
+}
